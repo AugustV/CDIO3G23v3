b03444079c5fe99059be55ae25c73bfc746f8d23
nikl2513 <nikl2513@outlook.dk>
2022-11-02 11:43:33 +0100

    name of the players start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         GUI gui = new GUI();
+        gui.showMessage("welcome to the game");
+        String userbutton = gui.getUserButtonPressed(
+                "how many players are in this game?",
+                "2", "3", "4"
+        );
+        int p = Integer.parseInt(userbutton);
+        if (p == 2){
+            String p1 = gui.getUserString("Enter the first players name");
+            String p2 = gui.getUserString("Enter the second players name");
+
+        }
+
     }
 }
