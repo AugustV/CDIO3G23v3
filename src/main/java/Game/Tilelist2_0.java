b07da00712cffd297e43de3c523f184b977d789b
nikl2513 <nikl2513@outlook.dk>
2022-11-07 16:07:49 +0100

    Tilelist2_0 start

--- src/main/java/Game/Tilelist2_0.java
+++ src/main/java/Game/Tilelist2_0.java
@@ -4,8 +4,11 @@ public class Tilelist2_0 {
     private Tile2_0 [] liste2_0;
 
     public Tilelist2_0() {
-        liste2_0 = new Tile2_0[40];
+        iste2_0 = new Tile2_0[40];
         int i = 0;
-        liste2_0[1] = new Tile2_0("start",-200,)
+        liste2_0[i++] = new Tile2_0("start",-200,0);
+        liste2_0[i++] = new Tile2_0("ejendom", 0,60);
+
+
     }
 }
