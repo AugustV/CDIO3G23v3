0f9abb4956f57cb00b104e04415ab0817f9302c0
fo123g <58248701+fo123g@users.noreply.github.com>
2022-11-23 10:18:36 +0100

    Final fixes

--- src/main/java/Game/Game.java
+++ src/main/java/Game/Game.java
@@ -12,32 +12,34 @@ class Game {
         boolean gameOver = false;   //sætter gameover til false til at starte med da ingen har tabt i starten af spillet
         PlayerTurn playerTurn = new PlayerTurn();   //opretter en player turn
         Turn1die die = new Turn1die();  //laver en tur med 1 terning
+
         do {    //så længe gameover er false køre den i loop
             for (Player player : players) { //
                 playerTurn.playerturn(player, die, gui);    //
-                if(player.GetSaldo() <= 0) {    //fortæller hvis playerens saldo er 0 eller mindre skal den ind i loopet
-                    gameOver =true; //ændre boolean gameover til true da en har tabt
+                if (player.GetSaldo() <= 0) {    //fortæller hvis playerens saldo er 0 eller mindre skal den ind i loopet
+                    gameOver = true; //ændre boolean gameover til true da en har tabt
                     break;  //spillet skal stoppe
                 }
             }
-        } while(gameOver == false); //køre do loopet mens gameover er false
-        for(Player player : players){
-            System.out.println(player.GetSaldo());
+        } while (gameOver == false); //køre do loopet mens gameover er false
 
-        }
-        Player winner = players[0];
-        for (Player player : players) {
+            for (Player player : players) {
+                System.out.println(player.GetSaldo());
 
-            if (player.GetSaldo() > winner.GetSaldo()) {
-                winner = player;
             }
-        }
-       gui.showMessage(winner.toString() + "has won the game ");
+            Player winner = players[0];
+            for (Player player : players) {
 
-        System.out.println(winner.toString() + " has won the game!" );
-        System.out.println("Congratulation to the winner");
-        System.out.println("And thank you for playing");
+                if (player.GetSaldo() > winner.GetSaldo()) {
+                    winner = player;
+                }
+            }
+            gui.showMessage(winner.toString() + "has won the game ");
+
+            System.out.println(winner.toString() + " has won the game!");
+            System.out.println("Congratulation to the winner");
+            System.out.println("And thank you for playing");
+
+        }
 
     }
-
-}
