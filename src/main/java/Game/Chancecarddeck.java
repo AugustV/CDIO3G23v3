b3d5b4cba4417427ed7d5e26d54be45a5ecd2eeb
Mikkel Amskov <mikkel@amskov.dk>
2022-11-23 10:31:44 +0100

    Rettelse af værdier i cancekort

--- src/main/java/Game/Chancecarddeck.java
+++ src/main/java/Game/Chancecarddeck.java
@@ -10,17 +10,17 @@ public class Chancecarddeck {
         int i = 0;
 
         deck[i++] = new Chancecard("Lottery","You win the lottery",5); //1
-        deck[i++] = new Chancecard("Federal Grant","Your business recieved a federal grant",10); //2
+        deck[i++] = new Chancecard("Federal Grant","Your business recieved a federal grant",3); //2
         deck[i++] = new Chancecard("Free Burger","You get a free burger coupon",1); //3
         deck[i++] = new Chancecard("Bad investment","You have lost money on an investment",-2);//4
         deck[i++] = new Chancecard("Counterfeitbills","Some of the cash you own turned out to be counterfeit",-1);//5
         deck[i++] = new Chancecard("Inflation","You have lost money on an investment",-1);//6
-        deck[i++] = new Chancecard("Gamestop","You got in on gamestop before the squeeze",20);//7
+        deck[i++] = new Chancecard("Gamestop","You got in on gamestop before the squeeze",8);//7
         deck[i++] = new Chancecard("Get out of jail","You can get out of prison next time you end up there",0);//8
-        deck[i++] = new Chancecard("Bad Exposure","Some of the bonds on your account go to zero",-10);//9
-        deck[i++] = new Chancecard("Big time investment","You gain an influx of capital from an investor ",10);//10
-        deck[i++] = new Chancecard("Gold reserves","Your gold ses a large appreciation",15);//11
-        deck[i++] = new Chancecard("Black Monday","The markets drop and you lost a vast amount of money",-20);//12
+        deck[i++] = new Chancecard("Bad Exposure","Some of the bonds on your account go to zero",-4);//9
+        deck[i++] = new Chancecard("Big time investment","You gain an influx of capital from an investor ",3);//10
+        deck[i++] = new Chancecard("Gold reserves","Your gold ses a large appreciation",6);//11
+        deck[i++] = new Chancecard("Black Monday","The markets drop and you lost a vast amount of money",-5);//12
 
     }
     public int getnumber(int i) {return deck[i].getNummer();}
